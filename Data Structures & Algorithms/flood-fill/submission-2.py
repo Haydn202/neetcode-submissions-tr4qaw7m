852003a48614a6d178fc5fcd410ba27dbd3fc67f
Haydn202 <75010392+Haydn202@users.noreply.github.com>
2026-04-21 16:14:34 +1200

    Add: flood-fill - submission-2

---
+++ Data Structures & Algorithms/flood-fill/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        intialColour = image[sr][sc]
+        maxR = len(image)
+        maxC = len(image[0])
+
+
+        def fill(colour: int, r: int, c: int):
+            if intialColour == color: 
+                return
+
+            if min(r, c) < 0:
+                return
+            
+            if r == maxR or c == maxC:
+                return
+
+            if image[r][c] != intialColour:
+                return
+            
+            image[r][c] = colour
+
+            fill(colour, r + 1, c)
+            fill(colour, r , c + 1)
+            fill(colour, r - 1, c)
+            fill(colour, r, c - 1)
+
+            return
+        
+        fill(color, sr, sc)
+        return image
+
+            
